465e6359597c2b7f3ad6f652cf0f75e2b84f0dbd
Conor Getty <conorgetty@gmail.com>
2023-05-23 15:31:19 -0500

    Phase 3 complete

--- Program.cs
+++ Program.cs
@@ -32,27 +32,31 @@ namespace Quest
 ",
                 4, 20
             );
+            //bool to play again or not
+            bool playAgain;
 
-            // "Awesomeness" is like our Adventurer's current "score"
-            // A higher Awesomeness is better
+            do
+            {
+                // "Awesomeness" is like our Adventurer's current "score"
+                // A higher Awesomeness is better
 
-            // Here we set some reasonable min and max values.
-            //  If an Adventurer has an Awesomeness greater than the max, they are truly awesome
-            //  If an Adventurer has an Awesomeness less than the min, they are terrible
-            int minAwesomeness = 0;
-            int maxAwesomeness = 100;
+                // Here we set some reasonable min and max values.
+                //  If an Adventurer has an Awesomeness greater than the max, they are truly awesome
+                //  If an Adventurer has an Awesomeness less than the min, they are terrible
+                int minAwesomeness = 0;
+                int maxAwesomeness = 100;
 
-            // Make a new "Adventurer" object using the "Adventurer" class
-            //Adventurer theAdventurer = new Adventurer("Jack");
-            //prompt user for their name
-            Console.Write("ARRRRRRG. What be ye name? ");
-            //create a new adventurer with the users first name
-            string name = Console.ReadLine();
-            Adventurer theAdventurer = new Adventurer(name);
+                // Make a new "Adventurer" object using the "Adventurer" class
+                //Adventurer theAdventurer = new Adventurer("Jack");
+                //prompt user for their name
+                Console.Write("ARRRRRRG. What be ye name? ");
+                //create a new adventurer with the users first name
+                string name = Console.ReadLine();
+                Adventurer theAdventurer = new Adventurer(name);
 
-            // A list of challenges for the Adventurer to complete
-            // Note we can use the List class here because have the line "using System.Collections.Generic;" at the top of the file.
-            List<Challenge> challenges = new List<Challenge>()
+                // A list of challenges for the Adventurer to complete
+                // Note we can use the List class here because have the line "using System.Collections.Generic;" at the top of the file.
+                List<Challenge> challenges = new List<Challenge>()
             {
                 twoPlusTwo,
                 theAnswer,
@@ -61,26 +65,31 @@ namespace Quest
                 favoriteBeatle
             };
 
-            // Loop through all the challenges and subject the Adventurer to them
-            foreach (Challenge challenge in challenges)
-            {
-                challenge.RunChallenge(theAdventurer);
-            }
+                // Loop through all the challenges and subject the Adventurer to them
+                foreach (Challenge challenge in challenges)
+                {
+                    challenge.RunChallenge(theAdventurer);
+                }
 
-            // This code examines how Awesome the Adventurer is after completing the challenges
-            // And praises or humiliates them accordingly
-            if (theAdventurer.Awesomeness >= maxAwesomeness)
-            {
-                Console.WriteLine("YOU DID IT! You are truly awesome!");
+                // This code examines how Awesome the Adventurer is after completing the challenges
+                // And praises or humiliates them accordingly
+                if (theAdventurer.Awesomeness >= maxAwesomeness)
+                {
+                    Console.WriteLine("YOU DID IT! You are truly awesome!");
+                }
+                else if (theAdventurer.Awesomeness <= minAwesomeness)
+                {
+                    Console.WriteLine("Get out of my sight. Your lack of awesomeness offends me!");
+                }
+                else
+                {
+                    Console.WriteLine("I guess you did...ok? ...sorta. Still, you should get out of my sight.");
+                }
+                Console.Write("Would ye like to play again for ye soule? (y/n): ");
+                string playAgainInput = Console.ReadLine();
+                playAgain = (playAgainInput.ToLower() == "y");
             }
-            else if (theAdventurer.Awesomeness <= minAwesomeness)
-            {
-                Console.WriteLine("Get out of my sight. Your lack of awesomeness offends me!");
-            }
-            else
-            {
-                Console.WriteLine("I guess you did...ok? ...sorta. Still, you should get out of my sight.");
-            }
+            while (playAgain);
         }
     }
 }
